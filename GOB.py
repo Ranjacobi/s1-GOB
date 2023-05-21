0a1a905de0ae35889992e3cbe2a46834e7355d08
Ranjacobi <48067432+Ranjacobi@users.noreply.github.com>
2023-05-21 13:58:14 +0300

    GOB.py

--- GOB.py
+++ GOB.py
@@ -1,16 +1,17 @@
+#S1-GOB was created by Ran Jacobi, 2023.
+# V1.5
+import shutil
 import subprocess
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
 import threading
-import shutil
-import os
-import sys
-from tkinter import ttk, filedialog, messagebox
+from tkinter import ttk, messagebox
 from getapi import APIClient
 from getexcel import ExcelProcessor
-import Pmw
-
+import os
+from datetime import datetime
+import sys
+from tkinter import *
+from tkmacosx import Button
 
 class TextRedirector:
     def __init__(self, widget, tag="stdout"):
@@ -22,24 +23,16 @@ class TextRedirector:
         self.widget.insert("end", str, (self.tag,))
         self.widget.configure(state="disabled")
         self.widget.yview("end")
+
 class GUIInput:
     def __init__(self):
         # Create GUI
         self.process_rawdata = None
         self.root = tk.Tk()
         self.root.title("S1-GOB: Guided On Boarding")
-        self.root.iconbitmap("./icons/icon.ico")
 
         self.progress_window = None  # Initialize progress_window attribute
         self.cancel_button = None
-
-        # Set application icon for different platforms
-        if os.name == 'nt':  # For Windows
-            icon_path = "./icons/icon.ico"
-            self.root.iconbitmap(default=icon_path)
-        elif os.name == 'posix':  # For macOS and Linux
-            icon_path = "./icons/icon.png"
-            self.root.iconphoto(True, tk.PhotoImage(file=icon_path))
 
         # Create notebook with tabs
         self.notebook = ttk.Notebook(self.root)
@@ -53,45 +46,42 @@ class GUIInput:
         self.tab1 = ttk.Frame(self.notebook)
         self.notebook.add(self.tab1, text="Grab CSV's")
 
-        # Create the "About" button
-        about_icon = tk.PhotoImage(file="./icons/info.png")
-        about_button = ttk.Button(self.tab1, command=self.about, image=about_icon)
-        about_button.pack(side=tk.TOP, padx=10, pady=10)
+        # Create a custom style for the button
+        style = ttk.Style()
+        style.configure('Purple.TButton', background='purple', foreground='black')
 
+        # Create a custom style for the button
+        style = ttk.Style()
+        style.configure('Purple.TButton', background='purple', foreground='black')
+
+        # Create the "About" button using tkmacosx:
+        about_button = Button(self.tab1, command=self.about, text="About", bg='purple', fg='black')
+        about_button.pack(side=tk.TOP, padx=10, pady=10)
 
         # Create balloon help for the API Token entry box
         self.token_label = ttk.Label(self.tab1, text="API Token:")
         self.token_label.pack(padx=10, pady=2)
         self.token_entry = ttk.Entry(self.tab1)
         self.token_entry.pack(padx=10, pady=2)
-        self.token_entry.insert(0, "dACAAjqKaQzosO4Zmw8dzGXAWLVA5xDP21ZnOtgwLlFztLsGXqfe8pyBQcuU55HMtDSzwjTAZOyTWrRT")
-        self.token_balloon = Pmw.Balloon(self.tab1)
-        self.token_balloon.bind(self.token_entry, "Enter your API token here.")
+        self.token_entry.insert(0, "")
 
         # Create balloon help for the Domain entry box
         self.domain_label = ttk.Label(self.tab1, text="Domain:")
         self.domain_label.pack(padx=10, pady=2)
         self.domain_entry = ttk.Entry(self.tab1)
         self.domain_entry.pack(padx=10, pady=2)
-        self.domain_entry.insert(0, "usea1-007.sentinelone.net")
-        self.domain_balloon = Pmw.Balloon(self.tab1)
-        self.domain_balloon.bind(self.domain_entry, "Enter your domain here.")
+        self.domain_entry.insert(0, "")
 
         # Create balloon help for the User entry box
         self.user_label = ttk.Label(self.tab1, text="User: (Optional)")
         self.user_label.pack(padx=10, pady=2)
         self.user_entry = ttk.Entry(self.tab1)
         self.user_entry.pack(padx=10, pady=2)
-        self.user_balloon = Pmw.Balloon(self.tab1)
-        self.user_balloon.bind(self.user_entry, "Enter your username here.")
 
-
-        # Create a custom style for the button
-        style = ttk.Style()
-        style.configure('Purple.TButton', foreground='white', background='purple', font=('Helvetica', 15), width=20)
-        create_start_button = ttk.Button(self.tab1, text="Start", style='Purple.TButton',
-                                         command=self.start_script)
-        create_start_button.pack(padx=10, pady=10)
+        # Create the "Start" button using tkmacosx:
+        self.create_start_button = Button(self.tab1, text="Start", bg='green', fg='white', font=('Helvetica', 15),
+                                          command=self.start_script)
+        self.create_start_button.pack(padx=20, pady=20)
 
         # Set permissions for the RawData folder
         try:
@@ -109,22 +99,16 @@ class GUIInput:
         refresh_button = ttk.Button(self.tab2, text="Refresh folder", command=self.refresh, style='Purple.TButton')
         refresh_button.pack(padx=10, pady=10, side=tk.TOP)
 
-
-
         # Create a label to show the files in the RawData folder
         label = ttk.Label(self.tab2, text="Files in the 'RawData' folder:")
         label.pack()
-
-        # Add a balloon to the label
-        label_balloon = Pmw.Balloon(self.tab2)
-        label_balloon.bind(label, "This box displays the list of files in the 'RawData' folder.\nYou can click to open the files")
 
         # Create a listbox to show the file names
         self.listbox = tk.Listbox(self.tab2, height=10, width=50)
         self.listbox.pack(padx=10, pady=10)
 
-        # Check if the RawData folder exists
-        raw_data_folder = "RawData"
+        # Check if the RawData folder exists under the user's home directory in S1GOB/RawData
+        raw_data_folder = os.path.expanduser("~/S1GOB/RawData")
         if os.path.exists(raw_data_folder):
             # Get the list of files in the folder
             files = os.listdir(raw_data_folder)
@@ -133,17 +117,15 @@ class GUIInput:
                 self.listbox.insert(tk.END, file)
                 # Add a binding to the file name to open it when double-clicked
                 self.listbox.bind('<Double-Button-1>',
-                             lambda event, f=file: subprocess.run(["open", os.path.join(raw_data_folder, f)]))
+                                  lambda event, f=file: subprocess.run(["open", os.path.join(raw_data_folder, f)]))
         else:
             # Show an error message if the folder doesn't exist
             self.listbox.insert(tk.END, "The 'RawData' folder does not exist.")
 
-        # Create a custom style for the button
-        style = ttk.Style()
-        style.configure('Purple.TButton', foreground='white', background='purple', font=('Helvetica', 15), width=20)
-        create_excel_button = ttk.Button(self.tab2, text="Create Excel File", style='Purple.TButton',
-                                         command=self.create_excel_file)
-        create_excel_button.pack(padx=10, pady=10)
+        # Create the "create_excel_file" button using tkmacosx:
+        create_excel_button = Button(self.tab2, text="Create Excel File", bg='green', fg='white', font=('Helvetica', 15),
+                                     borderless=1, command=self.create_excel_file)
+        create_excel_button.pack(padx=20, pady=20)
 
         # Create console-like area for output
         self.console = tk.Text(self.root, height=30, width=70)
@@ -159,13 +141,14 @@ class GUIInput:
     # Define the "About" function
     def about(self):
             # Create a message box with information about the application
-            messagebox.showinfo("About S1-GOB", "SS1-GOB is a tool that provides comprehensive health checks for customer's SentinelOne environment, enabling you to identify and present issues to your cusomter quickly and efficiently.\n\nVersion: 1.5.0\n\nCopyright 2023, Ran Jacobi")
+            messagebox.showinfo("About S1-GOB", "SS1-GOB is a tool that provides comprehensive health checks for customer's SentinelOne environment, enabling you to identify and present issues to your customer quickly and efficiently.\n\nVersion: 1.5.0\n\nCopyright 2023, Ran Jacobi")
+
     def refresh(self):
         # Clear listbox
         self.listbox.delete(0, tk.END)
 
-        # Check if the RawData folder exists
-        raw_data_folder = "RawData"
+        # Check if the RawData folder exists under the user's home directory in S1GOB/RawData
+        raw_data_folder = os.path.expanduser("~/S1GOB/RawData")
         if os.path.exists(raw_data_folder):
             # Get the list of files in the folder
             files = os.listdir(raw_data_folder)
@@ -174,24 +157,25 @@ class GUIInput:
                 self.listbox.insert(tk.END, file)
                 # Add a binding to the file name to open it when double-clicked
                 self.listbox.bind('<Double-Button-1>',
-                             lambda event, f=file: subprocess.run(["open", os.path.join(raw_data_folder, f)]))
+                                  lambda event, f=file: subprocess.run(["open", os.path.join(raw_data_folder, f)]))
         else:
             # Show an error message if the folder doesn't exist
             self.listbox.insert(tk.END, "The 'RawData' folder does not exist.")
 
     def create_excel_file(self):
         # Check if the raw data folder exists and isn't empty
-        if os.path.exists("RawData") and os.listdir("RawData"):
+        raw_data_folder = os.path.expanduser("~/S1GOB/RawData")
+        if os.path.exists(raw_data_folder) and os.listdir(raw_data_folder):
             # Check if the raw data folder contains any CSV files
-            csv_files = [f for f in os.listdir("RawData") if f.endswith(".csv")]
+            csv_files = [f for f in os.listdir(raw_data_folder) if f.endswith(".csv")]
             if csv_files:
                 # Create a new thread to run the ExcelProcessor
                 thread = threading.Thread(target=self.run_excel_processor)
                 thread.start()
             else:
-                print("No CSV files found in the raw data folder.")
+                print("No CSV files found in the 'RawData' folder.")
         else:
-            print("The raw data folder either doesn't exist or is empty.")
+            print("The 'RawData' folder either doesn't exist or is empty.")
 
     def run_excel_processor(self):
         # Create an ExcelProcessor object and process the CSV files
@@ -205,6 +189,7 @@ class GUIInput:
         self.cancel_button = None
 
     def start_script(self):
+        self.create_start_button.config(state=DISABLED)  # Disable the button after it is clicked
         self.token = self.token_entry.get()
         self.domain = self.domain_entry.get()
         self.user = self.user_entry.get()
@@ -212,10 +197,8 @@ class GUIInput:
         # Validate that required fields are not empty
         if not self.token or not self.domain:
             messagebox.showerror("Error", "Please enter both token and domain fields.")
+            self.create_start_button.config(state=NORMAL)
             return
-
-        # Display message that script is starting to run
-        messagebox.showinfo("S1-GOB", "The script is starting to run...")
 
         # Create progress bar
         progress_bar = ttk.Progressbar(self.root, mode='indeterminate')
@@ -241,16 +224,42 @@ class GUIInput:
 
         print(customer_endpoint, file=sys.stdout)
 
-        # create an object of the class
-        my_object = APIClient(token, domain, user)
-        # call the createLevelsDF() function on the object
-        level_account_df, level_site_df, level_group_df = my_object.createLevelsDF()
-        agents_df = my_object.httpGetPagination("/web/api/v2.1/agents")
-        policy_df = my_object.getAllPolicies(level_account_df, level_site_df, level_group_df)
-        users_df = my_object.httpGetPagination("/web/api/v2.1/users")
-        exclusions_df = my_object.getAllLevels("/web/api/v2.1/exclusions", level_account_df, level_site_df, level_group_df)
-        print(exclusions_df.columns.values.tolist())
-        print(exclusions_df)
+        try:
+            # Create an object of the class
+            my_object = APIClient(token, domain, user)
+
+            # Call the createLevelsDF() function on the object
+            level_account_df, level_site_df, level_group_df = my_object.createLevelsDF()
+
+            agents_df = my_object.httpGetPagination("/web/api/v2.1/agents")
+            policy_df = my_object.getAllPolicies(level_account_df, level_site_df, level_group_df)
+            users_df = my_object.httpGetPagination("/web/api/v2.1/users")
+            exclusions_df = my_object.getAllLevels("/web/api/v2.1/exclusions", level_account_df, level_site_df,
+                                                   level_group_df)
+            print(exclusions_df.columns.values.tolist())
+            print(exclusions_df)
+
+        except KeyError as e:
+            print("An error occurred: Data key not found. Please check your input.")
+            print("Possible reasons:")
+            print("- Invalid API token")
+            print("- Incorrect domain name format")
+            print("- VPN connection issue")
+            # Destroy progress bar
+            progress_bar.stop()
+            progress_bar.destroy()
+            self.create_start_button.config(state=NORMAL)
+
+        except UnboundLocalError as e:
+            # Suppress the specific error message
+            pass
+
+        except Exception as e:
+            # Suppress any other error messages
+            sys.stderr = open("/dev/null", "w")
+
+        # Restore the standard error output
+        sys.stderr = sys.__stderr__
 
         # installed_apps_df = getAllLevels("/web/api/v2.1/installed-applications",level_account_df,level_site_df, level_group_df)
         # print(installed_apps_df.columns.values.tolist())
@@ -261,25 +270,72 @@ class GUIInput:
         except KeyError:
             print("Unable to group by 'agentVersion' and 'osType'")
             agent_counts = None
-        try:
-            agent_counts.to_csv("agent_counts-" + domain + ".csv")
-        except AttributeError:
-            print("Error: 'agent_counts' is None")
 
-        agents_df.to_csv("agents-" + domain + ".csv")
-        users_df.to_csv("users-" + domain + ".csv")
-        policy_df.to_csv("policies-" + domain + ".csv")
-        exclusions_df.to_csv("exclusions-" + domain + ".csv")
-        # installed_apps_df.to_csv("installed_apps-"+domain+".csv")
+        if agent_counts is not None:
+            try:
+                agent_counts.to_csv(os.path.expanduser("~/agent_counts-{}.csv".format(domain)))
+            except AttributeError:
+                print("Error: 'agent_counts' is None")
 
-        # Create RawData folder if it does not exist
-        if not os.path.exists("RawData"):
-            os.makedirs("RawData")
+        agents_df.to_csv(os.path.expanduser("~/agents-" + domain + ".csv"))
+        users_df.to_csv(os.path.expanduser("~/users-" + domain + ".csv"))
+        policy_df.to_csv(os.path.expanduser("~/policies-" + domain + ".csv"))
+        exclusions_df.to_csv(os.path.expanduser("~/exclusions-" + domain + ".csv"))
+        # installed_apps_df.to_csv(os.path.expanduser("~/installed_apps-" + domain + ".csv"))
 
-        # Move all CSV files to RawData folder
-        for file in os.listdir():
-            if file.endswith(".csv"):
-                shutil.move(file, os.path.join("RawData", file))
+        # Create S1GOB folder if it does not exist
+        s1gob_folder = os.path.expanduser("~/S1GOB")
+        if not os.path.exists(s1gob_folder):
+            os.makedirs(s1gob_folder)
+
+        # Create RawData folder inside S1GOB folder if it does not exist
+        raw_data_folder = os.path.join(s1gob_folder, "RawData")
+        if not os.path.exists(raw_data_folder):
+            os.makedirs(raw_data_folder)
+
+        # Check if there are any files in the RawData folder
+        if len(os.listdir(raw_data_folder)) > 0:
+            # Create a new folder with the current date and time
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            new_folder_name = f"RawData_{timestamp}"
+            new_folder_path = os.path.join(s1gob_folder, new_folder_name)
+
+            # Ensure the new folder name is unique
+            counter = 1
+            while os.path.exists(new_folder_path):
+                new_folder_name = f"RawData_{timestamp}_{counter}"
+                new_folder_path = os.path.join(s1gob_folder, new_folder_name)
+                counter += 1
+
+            os.makedirs(new_folder_path)
+
+            # Move all files from RawData to the new folder
+            files = os.listdir(raw_data_folder)
+            for file in files:
+                file_path = os.path.join(raw_data_folder, file)
+                shutil.move(file_path, new_folder_path)
+
+            # Move the new folder one level below S1GOB
+            shutil.move(new_folder_path, s1gob_folder)
+
+        # Move all CSV files from the user's home directory to RawData folder
+        user_home_directory = os.path.expanduser("~")
+        csv_files = [file for file in os.listdir(user_home_directory) if file.endswith(".csv")]
+        failed_files = []
+        for file in csv_files:
+            file_path = os.path.join(user_home_directory, file)
+            destination_path = os.path.join(raw_data_folder, file)
+            try:
+                shutil.move(file_path, destination_path)
+                print(f"Moved '{file}' to '{destination_path}'")
+            except shutil.Error as e:
+                failed_files.append(file)
+                print(f"Error moving '{file}': {str(e)}")
+
+        if failed_files:
+            print("Error: Failed to move the following files to the RawData folder:")
+            for file in failed_files:
+                print(file)
 
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
         print("Agents DF:\n", agents_df)
@@ -289,19 +345,11 @@ class GUIInput:
         # print("Installed Apps\n",installed_apps_df)
         print("Exclusions DF:\n", exclusions_df)
         print("The Script is Done! The RawData folder is ready")
-
-        # Destroy progress bar
-        progress_bar.stop()
-        progress_bar.destroy()
-
-        # Use after() method to periodically update GUI
-        def update_gui():
-
-            # Display message box
-            messagebox.showinfo("Script Status", "Script finished successfully")
-
-        # Update GUI every 500 milliseconds
-        self.root.after(500, update_gui)
+        print("Location of RawData folder:", os.path.abspath(raw_data_folder))
+        if progress_bar is not None:
+            progress_bar.close()
+            progress_bar.clear()
+            self.create_start_button.config(state=NORMAL)
 
 gui = GUIInput()
 gui.root.mainloop()
